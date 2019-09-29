d851aea8c11e14955e71a435ece64a61184468b2
Jakub Bartusiak <jakub.bartusiak@gmail.com>
2019-09-29 19:23:48 +0200

    composite

---
+++ composite/src/main/java/pl/com/bartusiak/designpatterns/composite/Exercise.java
@@ -0,0 +1,54 @@
+package pl.com.bartusiak.designpatterns.composite;
+
+import java.util.*;
+import java.util.function.Consumer;
+
+interface ValueContainer extends Iterable<Integer> {}
+
+class SingleValue implements ValueContainer
+{
+    public int value;
+
+    // please leave this constructor as-is
+    public SingleValue(int value)
+    {
+        this.value = value;
+    }
+
+    @Override
+    public void forEach(Consumer<? super Integer> action) {
+        action.accept(value);
+    }
+
+    @Override
+    public Spliterator<Integer> spliterator() {
+        return Collections.singleton(value).spliterator();
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return Collections.singleton(value).iterator();
+    }
+}
+
+class ManyValues extends ArrayList<Integer> implements ValueContainer
+{
+}
+
+
+class MyList extends ArrayList<ValueContainer>
+{
+    // please leave this constructor as-is
+    public MyList(Collection<? extends ValueContainer> c)
+    {
+        super(c);
+    }
+
+    public int sum()
+    {
+        return stream().mapToInt(e->e.iterator().next()).sum();
+    }
+}
+
+public class Exercise {
+}
